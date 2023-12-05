59b82b11dcea85b59e4d837422eb7a2657a32eeb
Stacy <kozakanastasiia4@gmail.com>
2023-12-05 06:55:46 -0800

    final project

---
+++ finalproject.py
@@ -0,0 +1,27 @@
+# num = input().split(" ")
+# a , b = int(num[0]), int(num[1])
+# res = a * b * 0.5
+# print(f"{res:.2f}")
+
+# def area_of_triangle(a, b, c, d):
+#     area = (a + b + c +d)
+#     return area
+#
+# a = float(input("Введіть довжину a: "))
+# b = float(input("Введіть довжину b: "))
+# c = float(input("Введіть довжину c: "))
+# d = float(input("Введіть довжину d: "))
+# triangle_area = area_of_triangle(a, b, c, d)
+# print(f"сума дорівнює {triangle_area}")
+
+# def area_of_triangle(a, b):
+#     area = (a + b) * 2
+#     return area
+# a = float(input("Введіть довжину a: "))
+# b = float(input("Введіть довжину b: "))
+#
+# triangle_area = area_of_triangle(a, b)
+# print(f"площа прямокутника дорівнює {triangle_area}")
+
+
+
